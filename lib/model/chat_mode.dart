b23d15d3dd6682b7fae144accf287b228147fd3f
xfrogz <renattoyague@gmail.com>
2020-06-06 17:14:01 -0500

    Avance 1.3

---
+++ lib/model/chat_mode.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Persona 1",
+      message: "Holaaaaa...",
+      time: "15:21",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2020/06/05/07/58/apple-5261893_1280.png"),
+  new ChatModel(
+      name: "Persona 2",
+      message: "¿Que haces?",
+      time: "17:43",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2020/06/05/07/58/apple-5261893_1280.png"),
+  new ChatModel(
+      name: "Persona 3",
+      message: "En visto...",
+      time: "20:05",
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2020/06/05/07/58/apple-5261893_1280.png")
+];
