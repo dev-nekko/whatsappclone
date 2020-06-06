0e4b8683a10f5fffdb341823b89da335b6ec7924
xfrogz <renattoyague@gmail.com>
2020-06-06 16:12:14 -0500

    Avance 1.0

---
+++ whatsappclone/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappclone/whatsapp_home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "WhatsApp",
+      theme: new ThemeData(
+        primaryColor: new Color(0xFFD1F4E6),
+        accentColor: new Color(0xFF5DE1E6),
+      ),
+      home: new WhatsAppHome(),
+    );
+  }
+}
